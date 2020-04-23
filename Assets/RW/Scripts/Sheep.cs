81b38e581f5814059701a0d88a1ddf03150387a6
angelhd1999 <angelhd1999@gmail.com>
2020-04-23 13:17:41 +0200

    Sheep Falling Trigger feature added

--- Assets/RW/Scripts/Sheep.cs
+++ Assets/RW/Scripts/Sheep.cs
@@ -7,13 +7,17 @@ public class Sheep : MonoBehaviour
 
     public float runSpeed;
     public float gotHayDestroyDelay;
+    public float dropDestroyDelay;
 
     private bool hitByHay;
+    private Collider myCollider;
+    private Rigidbody myRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myCollider = GetComponent<Collider>();
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -24,17 +28,29 @@ public class Sheep : MonoBehaviour
 
     private void HitByHay()
     {
-        hitByHay = true; // 1
-        runSpeed = 0; // 2
-        Destroy(gameObject, gotHayDestroyDelay); // 3
+        hitByHay = true; 
+        runSpeed = 0; 
+        Destroy(gameObject, gotHayDestroyDelay); 
     }
 
-    private void OnTriggerEnter(Collider other) // 1
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Hay") && !hitByHay) // 2
+        if (other.CompareTag("Hay") && !hitByHay) 
         {
-            Destroy(other.gameObject); // 3
-            HitByHay(); // 4
+            Destroy(other.gameObject); 
+            HitByHay(); 
         }
+        else if (other.CompareTag("DropSheep"))
+        {
+            Drop();
+        }
+
     }
+    private void Drop()
+    {
+        myRigidbody.isKinematic = false; 
+        myCollider.isTrigger = false; 
+        Destroy(gameObject, dropDestroyDelay); 
+    }
+
 }
