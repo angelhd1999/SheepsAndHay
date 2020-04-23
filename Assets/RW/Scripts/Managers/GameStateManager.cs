b9f1977aa73bdf296e077710b55325def01467dd
angelhd1999 <angelhd1999@gmail.com>
2020-04-23 19:34:12 +0200

    New feature: User Interface

--- Assets/RW/Scripts/Managers/GameStateManager.cs
+++ Assets/RW/Scripts/Managers/GameStateManager.cs
@@ -35,16 +35,19 @@ public class GameStateManager : MonoBehaviour
     public void SavedSheep()
     {
         sheepSaved++;
+        UIManager.Instance.UpdateSheepSaved();
     }
     private void GameOver()
     {
         sheepSpawner.canSpawn = false;
         sheepSpawner.DestroyAllSheep();
+        UIManager.Instance.ShowGameOverWindow();
     }
 
     public void DroppedSheep()
     {
         sheepDropped++;
+        UIManager.Instance.UpdateSheepDropped();
 
         if (sheepDropped == sheepDroppedBeforeGameOver)
         {
