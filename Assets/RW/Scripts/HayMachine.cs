1a01cb63f3b59dc544da8afd03e3bb7fcf7357b0
angelhd1999 <angelhd1999@gmail.com>
2020-04-23 20:21:33 +0200

    New feature: Hay machine color change

--- Assets/RW/Scripts/HayMachine.cs
+++ Assets/RW/Scripts/HayMachine.cs
@@ -10,13 +10,38 @@ public class HayMachine : MonoBehaviour
     public GameObject hayBalePrefab; //Reference to the Hay Bale prefab.
     public Transform haySpawnpoint; //The point from which the hay will to be shot.
     public float shootInterval; //The smallest amount of time between shots
+    
+    public Transform modelParent;
+
+    public GameObject blueModelPrefab;
+    public GameObject yellowModelPrefab;
+    public GameObject redModelPrefab;
 
     private float shootTimer; //A timer that to keep track whether the machine can shoot or not
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadModel();
+    }
+    private void LoadModel()
+    {
+        Destroy(modelParent.GetChild(0).gameObject);
+
+        switch (GameSettings.hayMachineColor)
+        {
+            case HayMachineColor.Blue:
+                Instantiate(blueModelPrefab, modelParent);
+                break;
+
+            case HayMachineColor.Yellow:
+                Instantiate(yellowModelPrefab, modelParent);
+                break;
+
+            case HayMachineColor.Red:
+                Instantiate(redModelPrefab, modelParent);
+                break;
+        }
     }
 
     // Update is called once per frame
